7621135dd2c695ac7fbc3eb7d1fc81de176bf4a9
Santiago Risaro <santiago.risaro@grupoesfera.com.ar>
2019-07-08 16:33:47 -0300

    Usa gin para responder

--- index.go
+++ index.go
@@ -3,9 +3,21 @@ package handler
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 // ServeHTTP atiende pedidos y genera una respuesta
 func ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	router := gin.Default()
+	router.GET("", func(c *gin.Context) {
+		c.String(200, "Hola GIN")
+	})
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.ServeHTTP(w, r)
 	fmt.Fprintf(w, "<h1>Hello from Go on Now!</h1>")
 }
