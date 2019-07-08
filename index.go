9ffca10bb5d6724ec755e7b29adcb8ac60a79c8b
Santiago Risaro <santiago.risaro@grupoesfera.com.ar>
2019-07-08 18:16:13 -0300

    Inicializo el router una sola vez

--- index.go
+++ index.go
@@ -7,17 +7,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var router *gin.Engine
+
 // ServeHTTP atiende pedidos y genera una respuesta
 func ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	router := gin.Default()
-	router.GET("", func(c *gin.Context) {
-		c.String(200, "Hola GIN")
-	})
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+	if router == nil {
+		fmt.Println("El router es nulo")
+		router = gin.Default()
+		router.GET("", func(c *gin.Context) {
+			c.String(200, "Hola GIN")
 		})
-	})
+		router.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "pong",
+			})
+		})
+	}
 	router.ServeHTTP(w, r)
-	fmt.Fprintf(w, "<h1>Hello from Go on Now!</h1>")
 }
